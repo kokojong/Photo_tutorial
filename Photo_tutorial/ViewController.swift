f99fec1354abed4b31186c42c897722f3ed72cef
kokojong <duwhd0349@kookmin.ac.kr>
2021-09-27 16:29:14 +0900

    Initial Commit

---
+++ Photo_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo_tutorial
+//
+//  Created by kokojong on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
