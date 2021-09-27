1736df3b29cbd4d86536e31432675a21cf0e5f53
kokojong <duwhd0349@kookmin.ac.kr>
2021-09-27 23:32:58 +0900

    YPImagePicker 를 이용해서 프로필 사진 바꾸기 기능 개발 라이브러리 사용법에 대해 한번 더 익힘, cocoa pod 사용 한번더 익힘

--- Photo_tutorial/ViewController.swift
+++ Photo_tutorial/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import YPImagePicker
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var profileChangeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.profileImage.layer.cornerRadius = profileImage.frame.height / 2
+        self.profileChangeButton.layer.cornerRadius = 10
+        
+        self.profileChangeButton.addTarget(self, action: #selector(onProfileChangeButtonClicked), for: .touchUpInside)
+        
+        
     }
 
-
+    @objc fileprivate func onProfileChangeButtonClicked(){
+        print("onProfileChangeButtonClicked")
+        
+        var config = YPImagePickerConfiguration()
+        config.screens = [.library, .photo, .video]
+        
+        
+        let picker = YPImagePicker(configuration: config)
+        
+        // 사진이 선택되었을 때
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+                // 프사 변경
+                self.profileImage.image = photo.image
+            }
+            // 사진 선택창 닫기
+            picker.dismiss(animated: true, completion: nil)
+        }
+        // 사진 선택창 보여주기
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
 }
 
